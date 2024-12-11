4be932e928fb61f128288d99906731ff7d6beb69
Francisco <franciscotrianto@gmail.com>
2024-12-11 10:36:43 +0700

    feat: add unit test for drop table and delete record function

--- UnitTest.py
+++ UnitTest.py
@@ -3,10 +3,11 @@ import os
 import shutil
 import json
 import uuid
+import sys
 from lib.Schema import Schema
 from lib.Attribute import Attribute
+from lib.Condition import Condition
 from StorageManager import StorageManager
-from lib.Block import Block
 
 
 class TestStorageManager(unittest.TestCase):
@@ -31,7 +32,15 @@ class TestStorageManager(unittest.TestCase):
     def setUp(self):
         """
         Set up a clean StorageManager instance before each test.
+        Remove the information_schema_table.bin file if it exists and reinitialize StorageManager.
         """
+        information_schema_file = os.path.join(
+            self.TEST_BASE_PATH, "information_schema_table.bin"
+        )
+        if os.path.exists(information_schema_file):
+            os.remove(information_schema_file)
+
+        # Reinitialize the StorageManager after cleanup
         self.storage_manager = StorageManager(self.TEST_BASE_PATH)
 
     def generate_unique_table_name(self, base_name="test_table"):
@@ -131,39 +140,101 @@ class TestStorageManager(unittest.TestCase):
         self.assertGreater(stats[table_name]["b_r"], 0)  # Blocks should be > 0
         self.assertGreater(stats[table_name]["l_r"], 0)  # Tuple size should be > 0
 
-        # Ensure v_a_r is a dictionary and that each column has more than 0 unique values
         self.assertIsInstance(stats[table_name]["v_a_r"], dict)
         for column, unique_count in stats[table_name]["v_a_r"].items():
-            self.assertGreater(unique_count, 0, f"Column '{column}' should have more than 0 unique values.")
+            self.assertGreater(
+                unique_count,
+                0,
+                f"Column '{column}' should have more than 0 unique values.",
+            )
 
-    def test_set_and_get_index(self):
+    def test_delete_table(self):
         """
-        Test creating a hash index on a table column and retrieving records using the index.
+        Test if Table can be deleted.
         """
         table_name = self.generate_unique_table_name("test_table")
         schema = Schema([Attribute("id", "int", 4), Attribute("name", "varchar", 50)])
         self.storage_manager.create_table(table_name, schema)
 
-        # Insert data into the table
-        data = [(1, "Alice"), (2, "Bob"), (3, "Alice")]
-        self.storage_manager.insert_into_table(table_name, data)
+        # Verify the table exists
+        tables = self.storage_manager.list_tables()
+        self.assertIn(
+            table_name, tables, f"Table {table_name} should exist before deletion."
+        )
 
-        # Create a hash index on the 'name' column
-        self.storage_manager.set_index(table_name, "name", "hash")
+        self.storage_manager.delete_table(table_name)
 
-        # Check if the index file was created
-        index_file_path = os.path.join("./storage", f"{table_name}-name-hash.pickle")
-        print(index_file_path)
+        # check file system
+        table_file = os.path.join(self.TEST_BASE_PATH, f"{table_name}_table.bin")
+        self.assertFalse(
+            os.path.exists(table_file),
+            f"Table file {table_file} should have been deleted.",
+        )
 
-        self.assertTrue(os.path.isfile(index_file_path), "Hash index file was not created.")
+        # check instance
+        tables = self.storage_manager.list_tables()
+        self.assertNotIn(
+            table_name, tables, f"Table {table_name} should have been deleted."
+        )
 
-        # Retrieve records with 'name' = 'Alice' using the index
-        result = self.storage_manager.get_index(table_name, "name", "Alice", "varchar")
+        # Verify the table data is no longer accessible
+        with self.assertRaises(ValueError):
+            self.storage_manager.get_table_data(table_name)
 
-        # Expected records with 'name' = 'Alice'
-        expected = [(1, 'Alice'), (3, 'Alice')]
+    def test_delete_records(self):
+        """
+        Test if table can delete records using conditions.
+        """
+        table_name = self.generate_unique_table_name("test_table")
+        schema = Schema([Attribute("id", "int", 4), Attribute("name", "varchar", 50), Attribute("age", "int", 4)])
+        self.storage_manager.create_table(table_name, schema)
 
-        self.assertEqual(result, expected, "get_index did not return the expected records.")
+        # Verify the table exists
+        tables = self.storage_manager.list_tables()
+        self.assertIn(
+            table_name, tables, f"Table {table_name} should exist before deletion."
+        )
+
+        # insert records
+        self.storage_manager.insert_into_table(table_name, [(1, "Agus", 20), (2, "Bagas", 21), (3, "Ciko", 21), (4, "Dito", 21), (5, "Eko", 19)])
+
+        # check if insert correct
+        self.assertEqual(self.storage_manager.get_table_data(table_name), [(1, "Agus", 20), (2, "Bagas", 21), (3, "Ciko", 21), (4, "Dito", 21), (5, "Eko", 19)])
+
+        # delete records
+        self.storage_manager.delete_table_record(table_name, Condition("id", "=", "3"))
+
+        # check records
+        self.assertEqual(self.storage_manager.get_table_data(table_name), [(1, "Agus", 20), (2, "Bagas", 21), (4, "Dito", 21), (5, "Eko", 19)])
+
+        # delete records
+        self.storage_manager.delete_table_record(table_name, Condition("age", ">=", "20"))
+
+        # check records
+        self.assertEqual(self.storage_manager.get_table_data(table_name), [(5, "Eko", 19)])
+
+    def test_update_records(self):
+        """
+        Test if table can update records using conditions.
+        """
+        table_name = self.generate_unique_table_name("test_table")
+        schema = Schema([Attribute("id", "int", 4), Attribute("name", "varchar", 50), Attribute("age", "int", 4)])
+        self.storage_manager.create_table(table_name, schema)
+
+        # Verify the table exists
+        tables = self.storage_manager.list_tables()
+        self.assertIn(
+            table_name, tables, f"Table {table_name} should exist before deletion."
+        )
+
+        # insert records
+        self.storage_manager.insert_into_table(table_name, [(1, "Agus", 20), (2, "Bagas", 21), (3, "Ciko", 21), (4, "Dito", 21), (5, "Eko", 19)])
+
+        # check if insert correct
+        self.assertEqual(self.storage_manager.get_table_data(table_name), [(1, "Agus", 20), (2, "Bagas", 21), (3, "Ciko", 21), (4, "Dito", 21), (5, "Eko", 19)])
+
+        # update records
+        self.storage_manager.update_table_record(table_name, Condition("id", "=", "4"))
 
 
 class ColoredTextTestResult(unittest.TextTestResult):
@@ -174,14 +245,17 @@ class ColoredTextTestResult(unittest.TextTestResult):
     def addSuccess(self, test):
         super().addSuccess(test)
         print(f" {self.GREEN}SUCCESS: {test}{self.RESET}")
+        sys.stdout.flush()
 
     def addFailure(self, test, err):
         super().addFailure(test, err)
         print(f" {self.RED}FAIL: {test}{self.RESET}")
+        sys.stdout.flush()
 
     def addError(self, test, err):
         super().addError(test, err)
         print(f" {self.RED}ERROR: {test}{self.RESET}")
+        sys.stdout.flush()
 
 
 class ColoredTextTestRunner(unittest.TextTestRunner):
